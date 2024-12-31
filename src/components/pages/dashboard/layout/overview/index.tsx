5775ebe49181dbb03419f53f7e576a512c0f08ae
Youngmentor <olaleresuliton43@gmail.com>
2024-12-31 08:59:01 +0100

    update

--- src/components/pages/dashboard/layout/overview/index.tsx
+++ src/components/pages/dashboard/layout/overview/index.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import BalanceCard from "../overview/BalancedCard";
 import { useMerchant } from "../../../../../context/GetMerchant";
 import { formatNumber } from "../../../../../utils/Utils";
+import { useNavigate } from "react-router-dom";
 
 const Index: React.FC = () => {
+  const navigate = useNavigate()
   const { data } = useMerchant()
 
   return (
     <div className="w-full overflow-scroll max-[650px]:flex max-[650px]:flex-col max-[650px]:w-[100%] max-[650px]:mt-[40px]">
       <div className="flex gap-2 p-2 max-[650px]:flex-col max-[650px]:gap-[10px] max-[650px]:items-center max-[650px]:w-[100%]">
-        <BalanceCard title="Available Balance" balance={formatNumber(data?.balance)} />
-        <BalanceCard title="Total Withdraw" balance={data?.totalWithdrawals} />
-        <BalanceCard title="Total Products" balance={data?.totalProducts} />
-        <BalanceCard title="Total Orders" balance={data?.totalOrders} />
-        <BalanceCard title="Total Customers" balance={data?.totalCustomers} />
+        <BalanceCard title="Available Balance" balance={formatNumber(data?.balance)} onClick={()=> navigate('/dashboard/transaction')} />
+        <BalanceCard title="Total Withdraw" balance={data?.totalWithdrawals} onClick={()=> navigate('/dashboard/transaction')}/>
+        <BalanceCard title="Total Products" balance={data?.totalProducts} onClick={()=> navigate('/dashboard/store')}/>
+        <BalanceCard title="Total Orders" balance={data?.totalOrders} onClick={()=> navigate('/dashboard/order')}/>
+        <BalanceCard title="Total Customers" balance={data?.totalCustomers} onClick={()=> navigate('/dashboard/customer')}/>
       </div>
 
       <div className="flex items-center p-2 w-full gap-2 max-[650px]:flex-col">
